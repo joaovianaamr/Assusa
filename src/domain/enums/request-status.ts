e9d51689627522b70b9a6bd7a1d1b5fac935b46e
Assusa Dev <dev@assusa.com.br>
2026-01-10 18:39:55 -0300

    feat(domain): adiciona enum RequestStatus

---
+++ src/domain/enums/request-status.ts
@@ -0,0 +1,10 @@
+/**
+ * Status de uma solicitação
+ */
+export enum RequestStatus {
+  STARTED = 'STARTED',
+  SUCCESS = 'SUCCESS',
+  ERROR = 'ERROR',
+  NO_TITLES = 'NO_TITLES',
+  WAITING_USER = 'WAITING_USER',
+}
