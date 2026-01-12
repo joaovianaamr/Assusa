b7b0b9e2951f4435b352a4798dc0fc15bdc140fd
Joao vitor Viana <joaovianaamr@gmail.com>
2026-01-12 16:17:47 -0300

    chore(domain): remove arquivos ponte legados em domain/ports

--- src/domain/ports/conversation-state-store.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Ponte temporária durante migração
- * Reexporta o port do novo caminho
- * TODO: Migrar imports e remover este arquivo
- */
-export * from '../../application/ports/driven/conversation-state-store.js';
